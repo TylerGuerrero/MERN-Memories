c425c663e6e8716df48b05a358002322c4d27dd8
Tyler <guerrero.tyler3@gmail.com>
2021-06-13 17:07:16 -0400

    heroku setup

--- server/index.js
+++ server/index.js
@@ -47,9 +47,13 @@ app.use(session({
     store: store
 }))
 
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API')
+})
+
 app.use('/posts', postRoutes)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
 })
