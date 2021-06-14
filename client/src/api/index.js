1348ff5a1b96c5e24da16ae3194410ffa009c6f2
Tyler <guerrero.tyler3@gmail.com>
2021-06-13 21:18:25 -0400

    Added npm run dev script

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'http://localhost:5000/posts'
+const url = 'https://mern-memories-board.herokuapp.com/posts'
 
 export const fetchPost = async () => {
     return await axios.get(url)
